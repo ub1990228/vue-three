a325b861ee685f56bed712629b606c1378746c52
huangbin <1162954449@qq.com>
2021-02-04 14:50:33 +0800

    feat:add login vue

---
+++ src/components/login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <P>3D测试平台</P>
+    <input type="text" placeholder="用户名" v-model="loginform.name" required /><br><br>
+    <input type="password" placeholder="密码" v-model="loginform.password" required/><br><br>
+    <button type="submit" @click="login">登　录</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      loginform: {
+        name:'',
+        password:''
+      }
+    }
+  },
+  methods:{
+    login () {
+      if(this.loginform.name === 'admin' && this.loginform.password === '123456') {
+        this.$router.push('/test')
+      }
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Test from '@/components/test'
+import Login from '@/components/login'
 
 Vue.use(Router)
 
@@ -8,6 +9,11 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/test',
       name: 'test',
       component: Test
     }
